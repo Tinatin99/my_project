57db2ae67a4b1dd33af870d82c57457ce13e4355
Tinatin99 <tinatindavitadze7@gmail.com>
2025-03-29 22:23:08 +0400

    acordion.css

--- accordion.js
+++ accordion.js
@@ -2,7 +2,7 @@
 // 1. script mode "use strict"
 
 // "accordion.js"
-@@ -1,71 +1,77 @@
+// @@ -1,71 +1,77 @@
  
  
  
@@ -16,14 +16,17 @@
  // }
  
  // ლექციის შემდეგ დაწერილი კოდი
- let accordion = document.querySelectorAll('.accordion-container');
+
+
+
+//  let accordion = document.querySelectorAll('.accordion-container');
  
  
- accordion.forEach(item => {
-     item.addEventListener('click', function() {
-         this.classList.toggle('active');
-     });
- });
+//  accordion.forEach(item => {
+//      item.addEventListener('click', function() {
+//          this.classList.toggle('active');
+//      });
+//  });
  
  
  
@@ -42,116 +45,116 @@
  
  // სხვა სურათის ლინკები შემოვიტანე
  
- let data = [
-     {
-         id: 1,
-         title:'dried fruit',
-         imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/289e90b8-fa1c-4fb9-b90a-6758a016d650.jpeg',
-         title:'Tusheti',
-         imageUrl: 'https://www.turebi.ge/uploads/photos/tours1/large/62149_11.jpg?v=1',
+//  let data = [
+//      {
+//          id: 1,
+//          title:'dried fruit',
+//          imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/289e90b8-fa1c-4fb9-b90a-6758a016d650.jpeg',
+//          title:'Tusheti',
+//          imageUrl: 'https://www.turebi.ge/uploads/photos/tours1/large/62149_11.jpg?v=1',
  
-     },
-     {
-         id: 2,
-         title:'sweetpaste',
-         imageUrl: 'https://thumbs.dreamstime.com/z/traditional-georgian-sweets-called-churchkhela-churchela-candle-shaped-grape-juice-candy-nuts-inside-traditional-georgian-161801425.jpg',
-         title:'Mountain',
-         imageUrl: 'https://i.pinimg.com/736x/3c/27/5d/3c275d5f27a7d093c96a6fec825c9f7b.jpg',
+//      },
+//      {
+//          id: 2,
+//          title:'sweetpaste',
+//          imageUrl: 'https://thumbs.dreamstime.com/z/traditional-georgian-sweets-called-churchkhela-churchela-candle-shaped-grape-juice-candy-nuts-inside-traditional-georgian-161801425.jpg',
+//          title:'Mountain',
+//          imageUrl: 'https://i.pinimg.com/736x/3c/27/5d/3c275d5f27a7d093c96a6fec825c9f7b.jpg',
  
-     },
-     {
-         id: 3,
-         title:'churchkhela',
-         imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/65c4b4ec-6829-4d24-8c98-9171219e8ab8.jpeg',
-         title:'hello spring',
-         imageUrl: 'https://www.farmersalmanac.com/wp-content/uploads/2025/01/first-day-of-spring-2025.webp',
+//      },
+//      {
+//          id: 3,
+//          title:'churchkhela',
+//          imageUrl: 'https://images.bolt.eu/store/2022/2022-07-20/65c4b4ec-6829-4d24-8c98-9171219e8ab8.jpeg',
+//          title:'hello spring',
+//          imageUrl: 'https://www.farmersalmanac.com/wp-content/uploads/2025/01/first-day-of-spring-2025.webp',
  
-     },
-     {
-         id:4,
-         title:'flower',
-         imageUrl:'https://cdn.mos.cms.futurecdn.net/tXr5UjKDsDBrYBQM9znb2c-1200-80.jpg'
+//      },
+//      {
+//          id:4,
+//          title:'flower',
+//          imageUrl:'https://cdn.mos.cms.futurecdn.net/tXr5UjKDsDBrYBQM9znb2c-1200-80.jpg'
  
-     }
- 
- 
- 
- ];
- let sliderContainer = document.getElementById('slider')
- let arrowLeft =document.getElementById('arrow-left')
- let arrowRight =document.getElementById('arrow-right')
- let arrowLeftBtn =document.getElementById('arrow-left')
- let arrowRightBtn=document.getElementById('arrow-right')
- 
- let sliderIndex = 0
+//      }
  
  
  
- function createImg (item){
-     sliderContainer.style.backgroundImage = `url(${item.imageUrl})`
-     sliderContainer.style.backgroundRepeat= 'no-repeat'
- @@ -80,42 +86,138 @@
+//  ];
+//  let sliderContainer = document.getElementById('slider')
+//  let arrowLeft =document.getElementById('arrow-left')
+//  let arrowRight =document.getElementById('arrow-right')
+//  let arrowLeftBtn =document.getElementById('arrow-left')
+//  let arrowRightBtn=document.getElementById('arrow-right')
  
- }
+//  let sliderIndex = 0
  
  
  
- function setSlider(){
-     createImg(data[sliderIndex])
-     sliderContainer.innerText=''
-      createImg(data[sliderIndex])
-     let title= createH2tag(data[sliderIndex])
-     let dots = createDots()
+//  function createImg (item){
+//      sliderContainer.style.backgroundImage = `url(${item.imageUrl})`
+//      sliderContainer.style.backgroundRepeat= 'no-repeat'
+//  @@ -80,42 +86,138 @@
  
-     sliderContainer.appendChild(title)
-     sliderContainer.appendChild(dots)
-     let dotElements = dots.querySelectorAll('.dot')
-     dotElements[sliderIndex].classList.add('active') 
- }
+//  }
  
- function arrowLeft(){
-     if(sliderIndex === 0){
-         sliderIndex = data.length-1
-     }else{
-         sliderIndex --
-     }
-     setSlider()
- }
  
- function arrowRight(){
-     if(sliderIndex === data.length-1){
-         sliderIndex = 0
-     }else{
-         sliderIndex ++
-     }
-     setSlider()
- }
- arrowLeftBtn.addEventListener('click',arrowLeft)
- arrowRightBtn.addEventListener('click',arrowRight)
+ 
+//  function setSlider(){
+//      createImg(data[sliderIndex])
+//      sliderContainer.innerText=''
+//       createImg(data[sliderIndex])
+//      let title= createH2tag(data[sliderIndex])
+//      let dots = createDots()
+ 
+//      sliderContainer.appendChild(title)
+//      sliderContainer.appendChild(dots)
+//      let dotElements = dots.querySelectorAll('.dot')
+//      dotElements[sliderIndex].classList.add('active') 
+//  }
+ 
+//  function arrowLeft(){
+//      if(sliderIndex === 0){
+//          sliderIndex = data.length-1
+//      }else{
+//          sliderIndex --
+//      }
+//      setSlider()
+//  }
+ 
+//  function arrowRight(){
+//      if(sliderIndex === data.length-1){
+//          sliderIndex = 0
+//      }else{
+//          sliderIndex ++
+//      }
+//      setSlider()
+//  }
+//  arrowLeftBtn.addEventListener('click',arrowLeft)
+//  arrowRightBtn.addEventListener('click',arrowRight)
  
  // setInterval(()=>{
  //     arrowRight()
  // },2000)
  
- function createDots(){
-     let dots =document.createElement('div')
-     dots.classList.add('dots')
-     data.forEach((element)=>{
-         let dot = document.createElement('div')
-         dot.classList.add('dot')
-         dot.setAttribute('data-id',element.id-1)
-         dot.onclick= function(event){
-            let id = event.target.getAttribute('data-id')
-            sliderIndex = id
-            setSlider()
-         }
-         dots.appendChild(dot)
+//  function createDots(){
+//      let dots =document.createElement('div')
+//      dots.classList.add('dots')
+//      data.forEach((element)=>{
+//          let dot = document.createElement('div')
+//          dot.classList.add('dot')
+//          dot.setAttribute('data-id',element.id-1)
+//          dot.onclick= function(event){
+//             let id = event.target.getAttribute('data-id')
+//             sliderIndex = id
+//             setSlider()
+//          }
+//          dots.appendChild(dot)
  
-     })
+//      })
  
-     return dots
+//      return dots
  
- }
+//  }
  
  // function arrowLeft(){
  //     if(sliderIndex === 0){
@@ -160,7 +163,7 @@
  //         sliderIndex --
  //     }
  //     setSlider()
- setSlider()
+//  setSlider()
 
 
 
@@ -178,6 +181,7 @@
 // }
 
 // ლექციის შემდეგ დაწერილი კოდი
+
 let accordion = document.querySelectorAll('.accordion-container');
 
 
@@ -192,11 +196,17 @@ accordion.forEach(item => {
 // ეს ლექციაზე დაწერილი კოდია 
 
 
-accordion.forEach(item => {
-    item.addEventListener('click', function() {
-        this.classList.toggle('active');
-    });
-});
+// accordion.forEach(item => {
+//     item.addEventListener('click', function() {
+//         this.classList.toggle('active');
+//     });
+// });
+
+
+
+
+
+
 
 
 
@@ -315,6 +325,7 @@ setSlider()
 
 
 // registration form  validation-  რეგისტრაციის ფორმის ვალიდაცია
+
 
 document.getElementById('registration').addEventListener('submit',function(event){
     
