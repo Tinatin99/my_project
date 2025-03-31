2e7b07c1e71d0c7c3c10ee8ba357af06781b2adc
Tinatin99 <tinatindavitadze7@gmail.com>
2025-03-31 18:39:42 +0400

    add burger

--- debbuger.js
+++ debbuger.js
@@ -1,16 +1,16 @@
-// debugger
+debugger
 
-// let username = 'tinatin'
-// let age = 41
-// let info = username + ' ' +age
-// console.log(info);
-// let city = ['tbilis', 'gori']
-// function meet(){
+let username = 'tinatin'
+let age = 41
+let info = username + ' ' +age
+console.log(info);
+let city = ['zestafoni', 'batumi']
+function meet(){
     
-//     for(let i =0; i<city.length; i++ ){
-//         console.log("hello");
-//     }
+    for(let i =0; i<city.length; i++ ){
+        console.log("hello");
+    }
 
-// }
+}
 
-// meet()
+meet()
