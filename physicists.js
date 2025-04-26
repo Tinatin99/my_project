2be9014623bfd38cfe3560d2a7ce8c5c8ee5aa7a
Tinatin99 <tinatindavitadze7@gmail.com>
2025-04-26 15:22:53 +0400

    add imgs

--- physicists.js
+++ physicists.js
@@ -11,23 +11,5 @@ document.querySelector('.btn-toggle').addEventListener('click', function() {
   });
   
 
-//   document.querySelector('.btn-toggle').addEventListener('click', function() {
-//     const hiddenPart = document.querySelector('#hidden-part');
-    
-//     // თუ კარადებში არაფერი ჩანს, აჩვენე
-//     if (hiddenPart.classList.contains('hidden')) {
-//       hiddenPart.classList.remove('hidden'); // ხილვადობა
-//     } else {
-//       hiddenPart.classList.add('hidden'); // დამალვა
-//     }
-//   });
 
-//   document.querySelector('.btn-toggle').addEventListener('click', function() {
-//     const hiddenPart = document.querySelector('#hidden-part');
-    
-//     if (hiddenPart.classList.contains('hidden')) {
-//       hiddenPart.classList.remove('hidden');
-//     } else {
-//       hiddenPart.classList.add('hidden');
-//     }
-//   });
+
