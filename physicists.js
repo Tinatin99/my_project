6d87a33ded9deb4d870f6837cc6e72271a2c45e0
Tinatin99 <tinatindavitadze7@gmail.com>
2025-04-27 21:56:56 +0400

    poster.html

--- physicists.js
+++ physicists.js
@@ -1,15 +1,21 @@
 // ამ ფუნქციას მივმართავთ ღილაკზე დაჭერისას
-document.querySelector('.btn-toggle').addEventListener('click', function() {
-    const hiddenPart = document.querySelector('#hidden-part');
+// document.querySelector('.btn-toggle').addEventListener('click', function() {
+//     const hiddenPart = document.querySelector('#hidden-part');
     
-    // თუ კარადებში არაფერი ჩანს, აჩვენე
-    if (hiddenPart.classList.contains('hidden')) {
-      hiddenPart.classList.remove('hidden'); // ხილვადობა
-    } else {
-      hiddenPart.classList.add('hidden'); // დამალვა
-    }
-  });
+//     // თუ კარადებში არაფერი ჩანს, აჩვენე
+//     if (hiddenPart.classList.contains('hidden')) {
+//       hiddenPart.classList.remove('hidden'); // ხილვადობა
+//     } else {
+//       hiddenPart.classList.add('hidden'); // დამალვა
+//     }
+//   });
   
+
+  const btn = document.querySelector('.btn-toggle');
+  const hiddenPart = document.getElementById('hidden-part');
+  btn.addEventListener('click', () => {
+    hiddenPart.classList.toggle('hidden');
+  });
 
 
 
