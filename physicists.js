8ee0cdcd02381e0206200171d3ce3a8baaefe687
Tinatin99 <tinatindavitadze7@gmail.com>
2025-04-30 02:40:41 +0400

    ad js and img

--- physicists.js
+++ physicists.js
@@ -1,21 +1,31 @@
-// ამ ფუნქციას მივმართავთ ღილაკზე დაჭერისას
-// document.querySelector('.btn-toggle').addEventListener('click', function() {
-//     const hiddenPart = document.querySelector('#hidden-part');
-    
-//     // თუ კარადებში არაფერი ჩანს, აჩვენე
-//     if (hiddenPart.classList.contains('hidden')) {
-//       hiddenPart.classList.remove('hidden'); // ხილვადობა
-//     } else {
-//       hiddenPart.classList.add('hidden'); // დამალვა
-//     }
-//   });
-  
+// const hiddenPart = document.getElementById("hidden-part");
+// const showMoreBtn = document.getElementById("showMoreBtn");
 
-  const btn = document.querySelector('.btn-toggle');
-  const hiddenPart = document.getElementById('hidden-part');
-  btn.addEventListener('click', () => {
-    hiddenPart.classList.toggle('hidden');
-  });
+// showMoreBtn.addEventListener("click", () => {
+//   hiddenPart.classList.toggle("hidden");
+//   showMoreBtn.textContent = hiddenPart.classList.contains("hidden") ? "მეტის ჩვენება" : "დამალვა";
+// });
 
 
+const hiddenPart1 = document.getElementById("hidden-part1");
+const showMoreBtn1 = document.getElementById("showMoreBtn1");
 
+showMoreBtn1.addEventListener("click", () => {
+  hiddenPart1.classList.toggle("hidden");
+  showMoreBtn1.textContent = hiddenPart1.classList.contains("hidden") ? "მეტის ჩვენება" : "დამალვა";
+});
+
+const hiddenPart2 = document.getElementById("hidden-part2");
+const showMoreBtn2 = document.getElementById("showMoreBtn2");
+
+showMoreBtn2.addEventListener("click", () => {
+  hiddenPart2.classList.toggle("hidden");
+  showMoreBtn2.textContent = hiddenPart2.classList.contains("hidden") ? "მეტის ჩვენება" : "დამალვა";
+});
+
+
+document.getElementById("toggle-button").addEventListener("click", function () {
+  const hiddenPart = document.getElementById("hidden-part");
+  hiddenPart.classList.toggle("hidden");
+  this.textContent = hiddenPart.classList.contains("hidden") ? "მეტი მეცნიერები" : "ნაკლები მეცნიერები";
+});
